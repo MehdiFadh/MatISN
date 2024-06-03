de28c16cb527df2905886e492d3dd4647fa4c07a
Sofiane Ezzamari <sofiane.ezzamari@etu.univ-smb.fr>
2024-06-03 16:32:36 +0200

    connexion bd

---
+++ MatISN/ApplicationData.cs
@@ -0,0 +1,145 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatISN
+{
+    public class ApplicationData
+    {
+        private NpgsqlConnection connexion = null;
+
+
+        public NpgsqlConnection Connexion
+        {
+            get
+            {
+                return this.connexion;
+            }
+
+            set
+            {
+                this.connexion = value;
+            }
+        }
+
+        public ApplicationData()
+        {
+
+            this.ConnexionBD();
+            this.Read();
+        }
+        public void ConnexionBD()
+        {
+            try
+            {
+                Connexion = new NpgsqlConnection();
+                Connexion.ConnectionString = "Server=srv-peda-new;" +
+                                            "port=5433;" +
+                                            "Database=TD3;" +
+                                            "Search Path = SAE204;" +
+                                            "uid=ezzamaso;" +
+                                            "password=cAATY1;";
+                //à compléter dans les "" 
+                // @ sert à enlever tout pb avec les caractères 
+                Connexion.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("pb de connexion : " + e);
+                // juste pour le debug : à transformer en MsgBox 
+            }
+        }
+
+        public int Read()
+        {
+
+            String sql = "";
+            try
+            {
+                NpgsqlDataAdapter dataAdapter = new NpgsqlDataAdapter(sql, Connexion);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                foreach (DataRow res in dataTable.Rows)
+                {
+
+                }
+                return dataTable.Rows.Count;
+            }
+            catch (SqlException e)
+            { Console.WriteLine("pb de requete : " + e); return 0; }
+        }
+
+        public int Create( )
+        {
+            String sql = "";
+            try
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, Connexion);
+                int nb = cmd.ExecuteNonQuery();
+                return nb;
+                //nb permet de connaître le nb de lignes affectées par un insert, update, delete
+            }
+            catch (Exception sqlE)
+            {
+                Console.WriteLine("pb de requete : " + sql + "" + sqlE);
+                // juste pour le debug : à transformer en MsgBox 
+                return 0;
+            }
+        }
+
+        public int Update()
+        {
+            String sql = "";
+            try
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, Connexion);
+                int nb = cmd.ExecuteNonQuery();
+                return nb;
+                //nb permet de connaître le nb de lignes affectées par un insert, update, delete
+            }
+            catch (Exception sqlE)
+            {
+                Console.WriteLine("pb de requete : " + sql + "" + sqlE);
+                // juste pour le debug : à transformer en MsgBox 
+                return 0;
+            }
+
+        }
+
+        public int Delete()
+        {
+            String sql = "";
+            try
+            {
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, Connexion);
+                int nb = cmd.ExecuteNonQuery();
+                return nb;
+                //nb permet de connaître le nb de lignes affectées par un insert, update, delete
+            }
+            catch (Exception sqlE)
+            {
+                Console.WriteLine("pb de requete : " + sql + "" + sqlE);
+                // juste pour le debug : à transformer en MsgBox 
+                return 0;
+            }
+
+        }
+
+        public void DeconnexionBD()
+        {
+            try
+            {
+                Connexion.Close();
+            }
+            catch (Exception e)
+            { Console.WriteLine("pb à la déconnexion : " + e); }
+        }
+
+    }
+}
