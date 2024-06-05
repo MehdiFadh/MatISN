bb0b2c5d1849ffaa7f4f9137d39b0f2e85bd5dbc
Mehdi Fadhloune <mehdi.fadhloune@etu.univ-smb.fr>
2024-06-05 10:30:00 +0200

    Création des classe Fournisseur et Dispose et modification des autres classes

--- MatISN/DetailCaracteristique.cs
+++ MatISN/DetailCaracteristique.cs
@@ -8,5 +8,42 @@ namespace MatISN
 {
     internal class DetailCaracteristique
     {
-    }
+
+		private int numMateriel;
+
+		public int NumMateriel
+        {
+			get { return this.numMateriel; }
+			set { if(value == null)
+				{
+					throw new ArgumentNullException("Le numéro matériel est nul");
+				}
+				
+				this.numMateriel = value; }
+		}
+
+
+		private int numCaracteristique;
+
+		public int NumCaracteristique
+		{
+			get { return numCaracteristique; }
+			set {  if (value == null)
+                {
+                    throw new ArgumentNullException("Le numéro matériel est nul");
+                }
+
+                numCaracteristique = value; }
+		}
+
+		private string valeur;
+
+		public string Valeur
+		{
+			get { return this.valeur; }
+			set {this.valeur = value; }
+		}
+
+
+	}
 }
--- MatISN/DetailCommande.cs
+++ MatISN/DetailCommande.cs
@@ -8,5 +8,31 @@ namespace MatISN
 {
     internal class DetailCommande
     {
-    }
+
+		private int numCommande;
+
+		public int NumCommande
+		{
+			get { return this.numCommande; }
+			set { this.numCommande = value; }
+		}
+
+		private int numMatriel;
+
+		public int NumMateriel
+		{
+			get { return this.numMatriel; }
+			set { this.numMatriel = value; }
+		}
+
+		private int quantite;
+
+		public int Quantite
+		{
+			get { return this.quantite; }
+			set { this.quantite = value; }
+		}
+
+
+	}
 }
